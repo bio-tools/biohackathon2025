2ad1d113a2bfc1109bf1d756159a9dee67aff7a5
Sam Cox <sam.cox@hdruk.ac.uk>
2025-11-05 11:32:07 +0100

    Handle empty homepage

--- src/bridge/builders/github/github_transformer.py
+++ src/bridge/builders/github/github_transformer.py
@@ -35,6 +35,10 @@ class GitHubRepoTransformer(Transformer):
 
         repo_raw_data = raw_data.get("repo", {})
         latest_release_raw_data = raw_data.get("latest_release")
+
+        if not repo_raw_data["homepage"]:
+            repo_raw_data["homepage"] = None
+
         result = GitHubRepoModel(
             repo=FullRepository(**repo_raw_data),
             latest_release=Release(**latest_release_raw_data) if latest_release_raw_data else None,
