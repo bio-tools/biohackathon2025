84dde06f36f3262f656d3fb65e34fb3bdbb72a0f
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-06 10:06:08 +0100

    fix: passing the correct var into the mapper

--- src/bridge/pipelines/gh2bt_for_meta/main.py
+++ src/bridge/pipelines/gh2bt_for_meta/main.py
@@ -66,7 +66,7 @@ async def run(args: GitHubToBiotoolsForMetaPipelineArgs) -> BiotoolsToolModel:
     # response = await hf_provider.generate([message_sys, message])
 
     mapper = MapGitHub2BioTools(
-        repo=github_repo.repo,
+        repo=github_repo,
         metadata=args.existing_metadata,
     )
 
