32bcf62c7b43d1e313d2fd4a82800846ebeae669
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-06 20:39:46 +0100

    chore: add args to map func

--- src/bridge/pipelines/gh2bt_for_meta/map_funcs/language.py
+++ src/bridge/pipelines/gh2bt_for_meta/map_funcs/language.py
@@ -2,8 +2,11 @@
 Mapping functions for language field.
 """
 
+from bridge.core.biotools import LanguageEnum
+from bridge.core.github_languages import Language
 
-def map_language():
+
+def map_language(gh_languages: Language | None, bt_languages: list[LanguageEnum] | None) -> None:
     """
     Map languages from GitHub to bio.tools.
     """
