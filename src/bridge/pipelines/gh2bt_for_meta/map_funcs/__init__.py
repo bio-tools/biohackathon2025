273c8802d23b4bd7d585d6188db3d110097765ac
veitveit <veits@bmb.sdu.dk>
2025-11-06 14:57:19 +0100

    added function

--- src/bridge/pipelines/gh2bt_for_meta/map_funcs/__init__.py
+++ src/bridge/pipelines/gh2bt_for_meta/map_funcs/__init__.py
@@ -3,7 +3,9 @@ Individual mapping functions for GitHub to bio.tools.
 """
 
 from .homepage import map_homepage
+from .description import map_description
 
 __all__ = [
     "map_homepage",
+    "map_description",
 ]
