19ec17c1cf1a6d4fdf3cffaec39e92dad8ce01ef
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-11-06 12:48:48 +0100

    refactor

--- src/bridge/pipelines/gh2bt_for_meta/map_funcs/__init__.py
+++ src/bridge/pipelines/gh2bt_for_meta/map_funcs/__init__.py
@@ -2,11 +2,13 @@
 Individual mapping functions for GitHub to bio.tools.
 """
 
+from .documentation import map_documentation
 from .homepage import map_homepage
 from .description import map_description
 from .license import map_license
 
 __all__ = [
+    "map_documentation",
     "map_homepage",
     "map_description",
     "map_license",
