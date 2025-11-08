becb45fb379bb04d58928fa274ffaeb41667cf43
veitveit <veits@bmb.sdu.dk>
2025-11-08 15:04:56 +0100

    added edam topics to github topics

--- src/bridge/pipelines/bt2gh_for_pr/map_funcs/__init__.py
+++ src/bridge/pipelines/bt2gh_for_pr/map_funcs/__init__.py
@@ -4,12 +4,12 @@ Individual mapping functions for bio.tools to GitHub.
 
 from .citation import map_citation
 from .description import map_description
-from .function import map_function2topics
+from .edam import map_edam2topics
 from .homepage import map_homepage
 
 __all__ = [
     "map_citation",
     "map_description",
-    "map_function2topics",
+    "map_edam2topics",
     "map_homepage",
 ]
