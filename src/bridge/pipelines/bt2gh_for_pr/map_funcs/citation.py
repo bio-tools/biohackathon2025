1df4da8b37aab3050233cafccd4881031c42fb7d
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-27 14:33:10 +0100

    feat: employ new ref selection logic for cff

--- src/bridge/pipelines/bt2gh_for_pr/map_funcs/citation.py
+++ src/bridge/pipelines/bt2gh_for_pr/map_funcs/citation.py
@@ -8,49 +8,51 @@ The output is a valid `CITATION.cff` file that can be committed to a GitHub
 repository to enable software citation.
 """
 
-import sys
 from typing import Any
 
 import yaml
 
 from bridge.builders import compose_europe_pmc_metadata
 from bridge.core import Publication
-from bridge.core.biotools import PublicationItem, TypeEnum2
+from bridge.core.biotools import TypeEnum2
+from bridge.logging import get_user_logger
 from bridge.pipelines.utils import normalize_dict_strings, normalize_pydantic_model_strings, object_to_primitive
 
-# TODO: add logging
+logger = get_user_logger()
 
 TIMEOUT = 20
 
 
-def _require_primary_publications(bt_publication: list[PublicationItem] | None) -> list[PublicationItem]:
+def _compose_citation(
+    bt_params: dict[str, Any], references: list[Publication], primary_references: list[Publication]
+) -> dict[str, str]:
     """
-    Return the PublicationItem marked as Primary, if exists.
+    Generate a CITATION.cff dict from bio.tools metadata and references.
+    If there are no references, a minimal CITATION.cff is created.
+    If there are primary references, most recent one of them is selected as preferred citation.
+    Otherwise, the most recent reference is selected.
 
     Parameters
     ----------
-    meta : BiotoolsToolModel
-        The bio.tools tool metadata model.
+    bt_params : dict[str, Any]
+        The bio.tools tool relevant metadata as a dictionary.
+        Should contain:
+        - name - Name of the tool.
+        - biotoolsID - bio.tools identifier of the tool.
+        - homepage - Homepage URL of the tool.
+        - license - License of the tool.
+        - topic - List of topics associated with the tool.
+        - description - Description of the tool.
+    references : list[Publication]
+        List of all resolved Publication objects for the tool.
+    primary_references : list[Publication]
+        List of resolved Publication objects marked as Primary for the tool.
 
     Returns
     -------
-    PublicationItem
-        The PublicationItem marked as Primary.
-
-    Raises
-    ------
-    LookupError
-        If no Primary publication is found in the provided metadata.
+    dict[str, str]
+        A dictionary with the filename as key and the CITATION.cff content as value.
     """
-    pubs = bt_publication or []
-    primary = [p for p in pubs if p.type and TypeEnum2.Primary in p.type]
-    if not primary:
-        raise LookupError("No primary publication found in ToolModel.publication.")
-    return primary
-
-
-def _compose_citation(bt_params: dict[str, Any], references: list[Publication]):
-    """Generate a CITATION.cff dict from bio.tools metadata and references."""
     name = bt_params.get("name", None)
     biotools_id = bt_params.get("biotoolsID", None)
     homepage = bt_params.get("homepage", None)
@@ -74,9 +76,10 @@ def _compose_citation(bt_params: dict[str, Any], references: list[Publication]):
 
     if not references:
         base_cff["message"] = "If you use this software, please cite it using this CITATION.cff."
-        print("Resolved 0 Primary publications; creating CITATION.cff without publications.")
+        logger.added("No publications found in bio.tools. Creating CITATION.cff without publications.")
     else:
-        preferred = max(references, key=lambda r: (r.year or 0, r.title or ""))
+        selection_list_for_preferred = primary_references if primary_references else references
+        preferred = max(selection_list_for_preferred, key=lambda r: (r.year or 0, r.title or ""))
         base_cff.update(
             {
                 "message": ("If you use this software, please cite it and the Primary publications below."),
@@ -84,6 +87,7 @@ def _compose_citation(bt_params: dict[str, Any], references: list[Publication]):
                 "references": references,
             }
         )
+        logger.added(f"Added {len(references)} publication(s) to CITATION.cff.")
 
     primitive_cff = object_to_primitive(base_cff)
     return {"CITATION.cff": yaml.dump(primitive_cff, sort_keys=False, allow_unicode=True)}
@@ -91,7 +95,7 @@ def _compose_citation(bt_params: dict[str, Any], references: list[Publication]):
 
 async def map_citation(gh_citation_cff_exists: bool, bt_params: dict[str, Any]) -> dict[str, str]:
     """
-    Generate CITATION.cff content from the primary publications of a bio.tools tool.
+    Generate CITATION.cff content from the publications of a bio.tools tool.
     It uses Europe PMC to resolve publication metadata.
 
     Parameters
@@ -124,21 +128,23 @@ async def map_citation(gh_citation_cff_exists: bool, bt_params: dict[str, Any]) 
 
     bt_publication = bt_params.get("publication", None)
 
-    primary_publications = _require_primary_publications(bt_publication)
     references: list[Publication] = []
+    primary_references: list[Publication] = []
 
-    for primary_pub in primary_publications:
+    for pub in bt_publication or []:
         try:
             epmc_publication = await compose_europe_pmc_metadata(
-                pmid=primary_pub.pmid,
-                pmcid=primary_pub.pmcid,
-                doi=primary_pub.doi,
+                pmid=pub.pmid,
+                pmcid=pub.pmcid,
+                doi=pub.doi,
             )
             empc_publication_norm = normalize_pydantic_model_strings(epmc_publication)
             references.append(empc_publication_norm)
+            if pub.type and TypeEnum2.Primary in pub.type:
+                primary_references.append(empc_publication_norm)
         except Exception as e:
-            print(f"Warning: could not resolve {primary_pub}: {e}", file=sys.stderr)
+            logger.note(f"Could not resolve publication {pub}: {e}")
 
-    cff = _compose_citation(bt_params=bt_params, references=references)
+    cff = _compose_citation(bt_params=bt_params, references=references, primary_references=primary_references)
 
     return cff
