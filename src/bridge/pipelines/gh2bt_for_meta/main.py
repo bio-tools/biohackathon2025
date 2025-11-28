82c06ea18e84cb545af62ceddcb59d4344e086d2
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-28 21:47:51 +0100

    feat: wire in documentation

--- src/bridge/pipelines/gh2bt_for_meta/main.py
+++ src/bridge/pipelines/gh2bt_for_meta/main.py
@@ -80,6 +80,7 @@ async def run(args: GitHubToBiotoolsForMetaPipelineArgs) -> BiotoolsToolModel:
         language=await mapper.map["language"].run(),
         license=await mapper.map["license"].run(),
         version=await mapper.map["version"].run(),
+        documentation=await mapper.map["documentation"].run(),
     )
 
     biotools_url = settings.biotools_url
