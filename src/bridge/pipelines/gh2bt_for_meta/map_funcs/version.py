c03489773a38ed94faf3c2f17da371ef8f9d178a
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-28 23:24:05 +0100

    chore: use appropriate var names and type hints

--- src/bridge/pipelines/gh2bt_for_meta/map_funcs/version.py
+++ src/bridge/pipelines/gh2bt_for_meta/map_funcs/version.py
@@ -2,36 +2,38 @@
 Mapping releases for version metadata.
 """
 
+from bridge.core.biotools import VersionType
 from bridge.logging import get_user_logger
 
 logger = get_user_logger()
 
 
-def map_version(gh_version: str | None, bt_version: list | None) -> str | None:
+def map_version(gh_latest_version_tag: str | None, bt_versions: list[VersionType] | None) -> list[VersionType] | None:
     """
     Map GitHub releases smetadata to bio.tools version metadata.
     """
-    if not gh_version:
+    if not gh_latest_version_tag:
         # if no GitHub version, return bio.tools version, which may be None
-        if bt_version:
-            logger.conflict(f"GitHub has no version tag, but bio.tools has version '{bt_version}'")
+        if bt_versions:
+            logger.conflict(f"GitHub has no version tag, but bio.tools has version '{bt_versions}'")
         return None
 
-    if not bt_version:
+    if not bt_versions:
         # if no bio.tools version, return GitHub version as list
-        logger.added(f"version '{gh_version}'")
-        return [gh_version]
+        logger.added(f"version '{gh_latest_version_tag}'")
+        return [gh_latest_version_tag]
 
-    if gh_version not in bt_version:
-        if any(bt > gh_version for bt in bt_version):
+    if gh_latest_version_tag not in bt_versions:
+        if any(bt > gh_latest_version_tag for bt in bt_versions):
             # if any version in bt_version is newer than gh_version, consider conflict
             logger.conflict(
-                f"bio.tools version(s) '{bt_version}' is/are newer than GitHub latest version '{gh_version}'"
+                f"bio.tools version(s) '{bt_versions}' is/are newer than"
+                f" GitHub latest version '{gh_latest_version_tag}'"
             )
-            return [gh_version]
+            return [gh_latest_version_tag]
         # if both versions exist, but GitHub version not in bio.tools, add it
-        logger.added(f"version '{gh_version}' to existing bio.tools versions '{bt_version}'")
-        return bt_version + [gh_version]
+        logger.added(f"version '{gh_latest_version_tag}' to existing bio.tools versions '{bt_versions}'")
+        return bt_versions + [gh_latest_version_tag]
 
-    logger.exact(f"latest version '{bt_version}' already in bio.tools")
-    return bt_version
+    logger.exact(f"latest version '{bt_versions}' already in bio.tools")
+    return bt_versions
