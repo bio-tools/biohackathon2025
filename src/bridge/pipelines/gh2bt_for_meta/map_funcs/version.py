24d73ab921c6ca29aba196f8634a1218e0807e49
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-28 23:40:32 +0100

    chore: check version presence in bt more reliably

--- src/bridge/pipelines/gh2bt_for_meta/map_funcs/version.py
+++ src/bridge/pipelines/gh2bt_for_meta/map_funcs/version.py
@@ -12,29 +12,30 @@ def map_version(gh_latest_version_tag: str | None, bt_versions: list[VersionType
     """
     Map GitHub releases smetadata to bio.tools version metadata.
     """
-    latest_version_tag_as_bt = [VersionType(root=gh_latest_version_tag)]
-
     if not gh_latest_version_tag:
         # if no GitHub version, return bio.tools version, which may be None
-        logger.conflict("GitHub has no latest version tag, nothing to map")
+        logger.unchanged("GitHub has no latest version tag, nothing to map")
         return bt_versions
+
+    latest_version_tag_as_bt = VersionType(root=gh_latest_version_tag)
 
     if not bt_versions:
         # if no bio.tools version, return GitHub version as list
         logger.added(f"version '{gh_latest_version_tag}'")
-        return latest_version_tag_as_bt
+        return [latest_version_tag_as_bt]
 
-    if gh_latest_version_tag not in bt_versions:
-        if any(bt > gh_latest_version_tag for bt in bt_versions):
+    # if gh version not in bt versions
+    if not any(v.root == latest_version_tag_as_bt.root for v in bt_versions):
+        if any(bt > latest_version_tag_as_bt for bt in bt_versions):
             # if any version in bt_version is newer than gh_version, consider conflict
             logger.conflict(
                 f"bio.tools version(s) '{bt_versions}' is/are newer than"
                 f" GitHub latest version '{gh_latest_version_tag}'"
             )
-            return latest_version_tag_as_bt
+            return [latest_version_tag_as_bt]
         # if both versions exist, but GitHub version not in bio.tools, add it
         logger.added(f"version '{gh_latest_version_tag}' to existing bio.tools versions '{bt_versions}'")
-        return bt_versions + latest_version_tag_as_bt
+        return bt_versions + [latest_version_tag_as_bt]
 
-    logger.exact(f"latest version '{bt_versions}' already in bio.tools")
+    logger.exact(f"latest version '{gh_latest_version_tag}' already in bio.tools")
     return bt_versions
