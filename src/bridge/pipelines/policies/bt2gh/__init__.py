9ddf3e8ae7e24c3e6a27a4971180271578e61480
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-30 15:00:34 +0100

    fix: typo in policy name

--- src/bridge/pipelines/policies/bt2gh/__init__.py
+++ src/bridge/pipelines/policies/bt2gh/__init__.py
@@ -2,10 +2,10 @@
 Generic reconciliation policies for bio.tools to GitHub mapping.
 """
 
-from .reconcile_bt_optop_gh import reconcile_bt_optop_gh_issue
+from .reconcile_bt_ontop_gh import reconcile_bt_ontop_gh_issue
 from .reconcile_bt_over_gh import reconcile_bt_over_gh_issue
 
 __all__ = [
-    "reconcile_bt_optop_gh_issue",
+    "reconcile_bt_ontop_gh_issue",
     "reconcile_bt_over_gh_issue",
 ]
