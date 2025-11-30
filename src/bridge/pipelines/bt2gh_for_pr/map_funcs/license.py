a72c942121010b75d4ee7c59e6978a5ebc91b2cc
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-30 17:59:04 +0100

    feat: wire license mapping to the policy

--- src/bridge/pipelines/bt2gh_for_pr/map_funcs/license.py
+++ src/bridge/pipelines/bt2gh_for_pr/map_funcs/license.py
@@ -4,6 +4,7 @@ TODO
 
 from bridge.core.biotools import License as BTLicense
 from bridge.core.github_repo import License as GHLicense
+from bridge.pipelines.policies.bt2gh import reconcile_bt_over_gh
 from bridge.pipelines.utils import find_matching_enum_member
 
 
@@ -11,6 +12,15 @@ def map_license(gh_license: GHLicense | None, bt_license: BTLicense | None) -> d
     """
     TODO
     """
-    find_matching_enum_member(gh_license.spdx_id, BTLicense) if gh_license else None
+    gh_norm = find_matching_enum_member(gh_license.spdx_id, BTLicense) if gh_license else None
+    bt_norm = bt_license
 
-    return {"a": "b"}  # TODO: implement license mapping logic
+    def make_pr(bt_value: BTLicense) -> dict[str, str]:
+        return {"a": "b"}
+
+    return reconcile_bt_over_gh(
+        gh_norm=gh_norm,
+        bt_norm=bt_norm,
+        make_output=make_pr,
+        log_label="license",
+    )
