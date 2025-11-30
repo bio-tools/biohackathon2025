62591cab8e15e32779c9c3c7d5e75d09e3852db1
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2025-11-30 18:08:51 +0100

    feat: add a pr making function for license

--- src/bridge/pipelines/bt2gh_for_pr/map_funcs/license.py
+++ src/bridge/pipelines/bt2gh_for_pr/map_funcs/license.py
@@ -2,10 +2,15 @@
 TODO
 """
 
+from bridge.builders import compose_spdx_license_metadata
+from bridge.core import SPDXLicense
 from bridge.core.biotools import License as BTLicense
 from bridge.core.github_repo import License as GHLicense
+from bridge.logging import get_user_logger
 from bridge.pipelines.policies.bt2gh import reconcile_bt_over_gh
 from bridge.pipelines.utils import find_matching_enum_member
+
+logger = get_user_logger()
 
 
 def map_license(gh_license: GHLicense | None, bt_license: BTLicense | None) -> dict[str, str] | None:
@@ -16,7 +21,15 @@ def map_license(gh_license: GHLicense | None, bt_license: BTLicense | None) -> d
     bt_norm = bt_license
 
     def make_pr(bt_value: BTLicense) -> dict[str, str]:
-        return {"a": "b"}
+        spdx_id = bt_value.spdx_id
+        try:
+            license: SPDXLicense = compose_spdx_license_metadata(spdx_id)
+            full_text = license.full_text
+            logger.added(f"LICENSE.txt with full text for SPDX ID '{spdx_id}'")
+            return {"LICENSE.txt": full_text}
+        except Exception:
+            logger.unchanged(f"could not retrieve full text for SPDX ID '{spdx_id}'")
+            return None
 
     return reconcile_bt_over_gh(
         gh_norm=gh_norm,
