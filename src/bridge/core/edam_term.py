d2bc1b90c40c807762904044e98b0fce5dcbd615
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-06-01 14:59:49 +0200

    fix: correct type in edam term model

--- src/bridge/core/edam_term.py
+++ src/bridge/core/edam_term.py
@@ -17,7 +17,7 @@ class EDAMTerm(BaseModel):
         The short form of the EDAM term (e.g., "format_1930").
     label : str | None
         The preferred label of the EDAM term.
-    description : str | None
+    description : list[str] | str | None
         The description of the EDAM term, if available.
     narrow_synonym : list[str] | None
         A list of narrow synonyms for the EDAM term, if available.
@@ -30,5 +30,5 @@ class EDAMTerm(BaseModel):
     iri: str
     short_form: str
     label: str
-    description: str | None = None
+    description: list[str] | str | None = None
     narrow_synonym: list[str] | None = None
